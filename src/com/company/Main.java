7333dc1a43569ea888db8d8bfb7c05c03f488dd8
Dinar <xxxcorsair@ya.ru>
2017-09-22 11:02:19 +0300

    New MyArratDeque

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,13 +5,5 @@ public class Main {
 
     public static void main(String[] args) {
         MyArrayDeque my = new MyArrayDeque();
-        my.add(5);
-        my.add(3);
-        my.add(2);
-        System.out.println(my);
-        my.add(10);
-        System.out.println(my);
-        my.add(13);
-        System.out.println(my);
     }
 }
